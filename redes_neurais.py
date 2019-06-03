17fdf4345ec93777c1148a30c916963fb1169b5c
Victor Watanabe <victorhwmn@gmail.com>
2019-06-03 15:52:03 -0300

    Another fixing in redes_neurais.py

--- redes_neurais.py
+++ redes_neurais.py
@@ -32,6 +32,8 @@ def funcaoCusto_backp(nn_params, input_layer_size, hidden_layer_size, num_labels
     Theta1_grad = np.zeros(Theta1.shape)
     Theta2_grad = np.zeros(Theta2.shape)
     
+    eps = 1e-15
+
     #
     rotulos = np.zeros((len(y), num_labels), dtype=int) 
     for i in range(len(rotulos)):
@@ -65,7 +67,7 @@ def funcaoCusto_backp(nn_params, input_layer_size, hidden_layer_size, num_labels
         Theta1_grad = np.dot(delta2.T, a1)/m
         Theta2_grad = np.dot(delta3.T, a2)/m
      
-    J = np.sum(-rotulos*np.log(a3) - (1 - rotulos)*np.log(1 - a3))/m
+    J = np.sum(-rotulos*np.log(a3+eps) - (1 - rotulos)*np.log(1 - a3+eps))/m
 
     grad = np.concatenate([np.ravel(Theta1_grad), np.ravel(Theta2_grad)])
 
@@ -92,16 +94,16 @@ def predicao(Theta1, Theta2, X):
 def redes_neurais (Xtrain, Ytrain,Xval,Yval):
 
     input_layer_size  = Xtrain.shape[1]  # 20x20 dimensao das imagens de entrada
-    hidden_layer_size = 25   # 25 neuronios na camada oculta
+    hidden_layer_size = int((input_layer_size + 2)*2/3)   # 25 neuronios na camada oculta
     num_labels = 2          # 10 rotulos, de 1 a 10  
                          #  (observe que a classe "0" recebe o rotulo 10)
     epsilon_init = 0.12
     
     # carregando os pesos da camada 1
-    Theta1 =  np.random.rand(hidden_layer_size, 1 + input_layer_size) * 2 * epsilon_init - epsilon_init
+    Theta1 =  np.random.RandomState(10).rand(hidden_layer_size, 1 + input_layer_size) * 2 * epsilon_init - epsilon_init
     
     # carregando os pesos da camada 2
-    Theta2 = np.random.rand(num_labels, 1 + hidden_layer_size) * 2 * epsilon_init - epsilon_init
+    Theta2 = np.random.RandomState(10).rand(num_labels, 1 + hidden_layer_size) * 2 * epsilon_init - epsilon_init
     
     # concatena os pesos em um único vetor
     initial_rna_params = np.concatenate([np.ravel(Theta1), np.ravel(Theta2)])
